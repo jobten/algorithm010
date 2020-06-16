8ce6f136de6fd71a40e5c405e3ce23943dec9437
jobten <1017665401@qq.com>
2020-06-16 17:20:23 +0800

    upload 206

---
+++ Week01/[206]reverse-linked-list.js
@@ -0,0 +1,49 @@
+// 方法二：递归
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    if (head == null || head.next == null) {
+        return head
+    }
+    var listNode = reverseList(head.next)
+    head.next.next = head
+    head.next = null
+    return listNode
+};
+
+// 方法一：迭代
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+// var reverseList = function(head) {
+//     var prev = null
+//     var curr = head
+
+//     while(curr !== null) {
+//         var nextTemp = new ListNode(-1)
+//         nextTemp= curr.next
+//         curr.next = prev
+//         prev = curr
+//         curr = nextTemp
+//     }
+//     return prev
+// };
